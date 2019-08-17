15d74dc7138a7b06cc7bb9f149eb1e871d323b65
Milan Felix Šulc <rkfelix@gmail.com>
2019-08-17 17:54:47 +0200

    Examples: reunite to php & pure example

--- examples/php-ext/ds.php
+++
@@ -1,6 +0,0 @@
-<?php
-$map = new \Ds\Map(["a" => 1, "b" => 2, "c" => 3]);
-$map->apply(function($key, $value) { return $value * 2; });
-
-print_r($map);
-?>
---
+++ examples/php/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>juicyfx/now-builders</title>
+</head>
+<body>
+    <h1>Multiple PHP versions</h1>
+
+    <ul>
+        <li><a href="/php72.php">PHP 7.2</a></li>
+        <li><a href="/php73.php">PHP 7.3</a></li>
+        <li><a href="/php74.php">PHP 7.4</a></li>
+    </ul>
+
+    <h1>Multiple PHP running modes</h1>
+
+    <ul>
+        <li><a href="/cli.php">CLI</a></li>
+        <li><a href="/cgi.php">CGI</a></li>
+        <li><a href="/fpm.php">FPM</a></li>
+        <li><a href="/server.php">SERVER</a></li>
+    </ul>
+
+    <h1>now-php</h1>
+
+    <p>Take a look at new community builder <a href="https://github.com/juicyfx/now-php">now-php</a></p>
+
+    <hr>
+
+    Copyright &copy; <?=date('Y')?> | <a href="https://f3l1x.io">f3l1x</a>
+</body>
+</html>
